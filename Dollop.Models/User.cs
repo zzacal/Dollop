c9081a732909aa7d21a09079ed54e85793214493
Zac Zacal <Jizacal@outlook.com>
2020-07-10 18:50:56 -0700

    Added Models and Api Projects

---
+++ Dollop.Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dollop.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string DisplayName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
