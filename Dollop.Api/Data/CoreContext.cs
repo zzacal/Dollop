6190b6c42dbc9dde5166a07f73e24533c6563a12
Zac Zacal <Jizacal@outlook.com>
2020-07-10 18:55:36 -0700

    Added UsersController

---
+++ Dollop.Api/Data/CoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Dollop.Models;
+
+namespace Dollop.Api.Data
+{
+    public class CoreContext : DbContext
+    {
+        public CoreContext (DbContextOptions<CoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Dollop.Models.User> User { get; set; }
+    }
+}
